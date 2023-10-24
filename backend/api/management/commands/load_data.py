5f6cf110cf53d926314af0509663d431613b28f4
AlexAvdeev1986 <116315897+AlexAvdeev1986@users.noreply.github.com>
2023-10-25 01:33:14 +0300

    111111111
    111111111111

--- backend/api/management/commands/load_data.py
+++ backend/api/management/commands/load_data.py
@@ -1,5 +1,7 @@
-from csv import DictReader
 import asyncio
+from csv import DictReader
+from asgiref.sync import sync_to_async
+
 from django.core.management.base import BaseCommand
 from recipes.models import Ingredient
 
@@ -12,7 +14,19 @@ csv_files = [
 ]
 
 class Command(BaseCommand):
-    """Загружает ингредиенты из файла csv."""
+    help = "Загружает ингредиенты из файла csv."
+
+    async def async_update_or_create(self, model, row):
+        try:
+            # Using sync_to_async to turn the synchronous Django ORM operation into an async one
+            await sync_to_async(model.objects.update_or_create, thread_sensitive=True)(
+                name=row["name"], defaults=row
+            )
+            return True
+        except Exception as error:
+            print(row)
+            print(f"Ошибка записи в таблицу модели {model.__name__}, {str(error)}")
+            return False
 
     async def csv_loader(self, cf):
         csv_file = "static/data/ingredients.csv"
@@ -20,30 +34,30 @@ class Command(BaseCommand):
             reader = DictReader(csvfile, fieldnames=cf["fieldnames"])
             print(f'Загрузка в таблицу модели {cf["model"].__name__}')
 
-        i, err, r = 0, 0, 0
+            tasks = []
+            i, err, r = 0, 0, 0
 
-        for row in reader:
-            try:
-                await cf["model"].objects.update_or_create(
-                    name=row["name"], defaults=row
-                )
-                r += 1
-            except Exception as error:
-                print(row)
-                print(
-                    f"Ошибка записи в таблицу модели "
-                    f'{cf["model"].__name__}, '
-                    f"{str(error)}"
-                )
-                err += 1
-            i += 1
-        print(
-            f"Всего: {i} строк. Загружено: {r} строк. "
-            f"Ошибки: {err} строк."
-        )
+            async for row in reader:
+                # Creating a task for each row to be inserted
+                task = asyncio.create_task(self.async_update_or_create(cf["model"], row))
+                tasks.append(task)
 
-    async def handle(self, *args, **options):
+            # Waiting for all tasks to complete
+            for task in asyncio.as_completed(tasks):
+                success = await task
+                if success:
+                    r += 1
+                else:
+                    err += 1
+                i += 1
+
+            print(f"Всего: {i} строк. Загружено: {r} строк. Ошибки: {err} строк.")
+
+    async def async_handle(self, *args, **options):
         print("Идет загрузка данных...")
-        tasks = [self.csv_loader(cf) for cf in csv_files]
-        await asyncio.gather(*tasks)
+        for cf in csv_files:
+            await self.csv_loader(cf)
         print("Загрузка завершена.")
+
+    def handle(self, *args, **options):
+        asyncio.run(self.async_handle(*args, **options))
