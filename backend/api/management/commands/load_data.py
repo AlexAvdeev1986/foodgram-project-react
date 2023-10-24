60d70e6636405f6f3ef856960876c4a8e8512faa
AlexAvdeev1986 <116315897+AlexAvdeev1986@users.noreply.github.com>
2023-10-25 01:29:28 +0300

    111
    11111

--- backend/api/management/commands/load_data.py
+++ backend/api/management/commands/load_data.py
@@ -1,5 +1,5 @@
 from csv import DictReader
-
+import asyncio
 from django.core.management.base import BaseCommand
 from recipes.models import Ingredient
 
@@ -11,40 +11,39 @@ csv_files = [
     },
 ]
 
-
 class Command(BaseCommand):
     """Загружает ингредиенты из файла csv."""
 
-    def csv_loader(self, cf):
+    async def csv_loader(self, cf):
         csv_file = "static/data/ingredients.csv"
         with open(csv_file, encoding="utf-8", newline="") as csvfile:
             reader = DictReader(csvfile, fieldnames=cf["fieldnames"])
             print(f'Загрузка в таблицу модели {cf["model"].__name__}')
 
-            i, err, r = 0, 0, 0
+        i, err, r = 0, 0, 0
 
-            for row in reader:
-                try:
-                    cf["model"].objects.update_or_create(
-                        name=row["name"], defaults=row
-                    )
-                    r += 1
-                except Exception as error:
-                    print(row)
-                    print(
-                        f"Ошибка записи в таблицу модели "
-                        f'{cf["model"].__name__}, '
-                        f"{str(error)}"
-                    )
-                    err += 1
-                i += 1
-            print(
-                f"Всего: {i} строк. Загружено: {r} строк. "
-                f"Ошибки: {err} строк."
-            )
+        for row in reader:
+            try:
+                await cf["model"].objects.update_or_create(
+                    name=row["name"], defaults=row
+                )
+                r += 1
+            except Exception as error:
+                print(row)
+                print(
+                    f"Ошибка записи в таблицу модели "
+                    f'{cf["model"].__name__}, '
+                    f"{str(error)}"
+                )
+                err += 1
+            i += 1
+        print(
+            f"Всего: {i} строк. Загружено: {r} строк. "
+            f"Ошибки: {err} строк."
+        )
 
-    def handle(self, *args, **options):
+    async def handle(self, *args, **options):
         print("Идет загрузка данных...")
-        for сf in csv_files:
-            self.csv_loader(сf)
+        tasks = [self.csv_loader(cf) for cf in csv_files]
+        await asyncio.gather(*tasks)
         print("Загрузка завершена.")
