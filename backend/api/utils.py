20eb6e924e149d9502f8d1847a5f238240e27d5e
AlexAvdeev1986 <116315897+AlexAvdeev1986@users.noreply.github.com>
2023-10-30 07:03:36 +0300

    1214545
    1243456y

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -7,7 +7,7 @@ def ingredient_amount_set(recipe, ingredients_data):
     """Создает связи рецепта с количеством ингредиента."""
     for ingredient_data in ingredients_data:
         id = ingredient_data.get("id")
-        ingredient_id = get_object_or_404(Ingredient, id=id)
+        ingredient = get_object_or_404(Ingredient, id=id)
         amount = ingredient_data.get("amount")
         IngredientAmount.objects.create(
             recipe=recipe, ingredient=ingredient_id, amount=amount
