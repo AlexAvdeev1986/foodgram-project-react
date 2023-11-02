40152a5049e1b7055b6cb165530bf21b7541f5a8
AlexAvdeev1986 <116315897+AlexAvdeev1986@users.noreply.github.com>
2023-11-02 05:54:33 +0300

    2345543

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -224,8 +224,7 @@ class RecipeWriteSerializer(serializers.ModelSerializer):
 
     def update(self, instance, validated_data):
         """Обновляет существующий рецепт."""
-        instance.name = validated_data.get("name", instance.name)
-        instance.text = validated_data.get("text", instance.text)
+        instance.__dict__.update((key, value) for key, value in validated_data.items() if key in {'name', 'text'})
         tags_data = validated_data.pop("tags")
         ingredients_data = validated_data.pop("ingredients")
         instance.tags.set(tags_data)
