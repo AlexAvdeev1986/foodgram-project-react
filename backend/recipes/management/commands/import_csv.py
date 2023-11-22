95319ccf5147135c3fa49c8d40b012789d890e03
AlexAvdeev1986 <116315897+AlexAvdeev1986@users.noreply.github.com>
2023-11-22 07:27:02 +0300

    34242432
    24524643

--- backend/recipes/management/commands/import_csv.py
+++ backend/recipes/management/commands/import_csv.py
@@ -2,11 +2,8 @@
 Import of ingredients from *.csv file to the database.
 """
 import csv
-
 from django.core.management.base import BaseCommand
-
 from recipes.models import Ingredient
-
 
 class Command(BaseCommand):
     help = 'Import ingredients from csv file'
@@ -20,7 +17,11 @@ class Command(BaseCommand):
             reader = csv.reader(f)
             for row in reader:
                 name, measurement_unit = row
-                ingredient = Ingredient.objects.create(
+                # Проверка наличия ингредиента перед созданием
+                ingredient, created = Ingredient.objects.get_or_create(
                     name=name, measurement_unit=measurement_unit
                 )
-                ingredient.save()
+                if created:
+                    self.stdout.write(self.style.SUCCESS(f'Successfully imported ingredient: {name}'))
+                else:
+                    self.stdout.write(self.style.SUCCESS(f'Ingredient {name} already exists. Skipping.'))
